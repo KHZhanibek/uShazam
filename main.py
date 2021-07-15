222c29fbc841061a5622969e818cf3a54124019d
Narkhan Kamzabek <nkamzabek@yandex-team.ru>
2021-07-15 20:05:41 +0300

    u must commit it

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('1847617795:AAGC9PPTMvbl9FeoGG2-4gcywEgAB2PCYGE');
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-	bot.send_message(message.chat.id, "Hello daun! Print /game for playing")
+	bot.send_message(message.chat.id, "Hello Zhanibek daun! Print /game for playing")
 @bot.message_handler(commands=['game'])
 def game_command(message):
 	bot.send_message(message.chat.id, "Processing music...")
